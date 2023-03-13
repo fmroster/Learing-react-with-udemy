85339d7f29bcb75057bbd2ef0e4b81629cb32eb3
fmroster <frankiemooketsi@gmail.com>
2023-03-13 14:14:17 +0200

    try

--- jsx/src/App.jsx
+++ jsx/src/App.jsx
@@ -5,12 +5,48 @@ import cortana from './images/cortana.png'
 import siri from './images/siri.png'
 function App() {
   return (
-    <div className="">
-        <div className="App-header">Personal digital assistance</div>
-        <ProfileCard  title ="Alexa" handle="@alexa" image={Alexa} />
-        <ProfileCard title ="cortana" handle="@coratana" image ={cortana}/>
-        <ProfileCard title ="Siri" handle="@siri01" image={siri}/>
+    <div>
+        <div className="hero is-primary">
+            <div className="hero-body">
+                <p className="title">
+                    Personal digital assistance
+                </p>
+            </div>
+        </div>
+        <div className="container">
+            <div className="section">
+                <div className="columns">
+                    
+                <div className="column is-4">
+                     <ProfileCard  
+                        title ="Alexa" 
+                        handle="@alexa" 
+                        image={Alexa}
+                        desc="hello these is aleaxa" 
+                        />
 
+                </div>
+                <div className="column is-4">
+                    <ProfileCard 
+                        title ="cortana" 
+                        handle="@coratana" 
+                        image ={cortana}
+                        desc="cortana by miscrosft"
+                        />
+
+                </div>
+                <div className="column is-4">
+                     <ProfileCard 
+                        title ="Siri" 
+                        handle="@siri01" 
+                        image={siri}
+                        desc="apple powered"
+                        />
+
+                </div>
+                </div>
+            </div>
+        </div>
     </div>
     )
 }
--- jsx/src/component/Profilecard.jsx
+++ jsx/src/component/Profilecard.jsx
@@ -1,16 +1,33 @@
-function ProfileCard ({title, handle, image}){
+function ProfileCard ({title, handle, image, desc}){
     // 1
     // const title = props.title;
     // const handle = props.handle
 
     // 2
     // const {title , handle } = props;
-    console.log(title, handle, image)
+    console.log(title, handle, image, desc)
     return(
-        <div>
-            {title }
-            {handle}
-            <img src={image} alt={image} />
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt={image} />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">
+                        {title}
+                    </p>
+                    <p className="subtitle is-6">
+                        {handle}
+                    </p>
+                </div>
+                <div className="content">
+                    {desc}
+                </div>
+                
+            </div>
+            
         </div>
     )
 }
